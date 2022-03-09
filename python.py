4bdb90914c301947c5e937feac5ec318ddc3e012
Shayak Rakshit <85937567+iamgithum@users.noreply.github.com>
2022-03-09 21:09:06 +0530

    Update python.py

--- python.py
+++ python.py
@@ -81,3 +81,87 @@ print(range)
 for j in range:
   print(j)
 
+## List
+list = [9, 18, 27, 36, 45]    ## Declaring List
+print(list)                   ## Printing List
+x = 0
+while x < 5:
+    print(list[x])            ## Accessing list items
+    x = x + 1
+
+z = 0
+for y in list:                ## Modifying Lists 
+    z = z + 1
+    if y == 18:
+        list[z] = 54
+        print(list)
+
+## Appending & Insert
+example = [3, 6, 9, 12, 15, 18]
+example.append(21)
+print(example)
+
+example.insert(3, 24)
+print(example)
+## shortening list
+example = [3, 6, 9, 12, 15, 18]
+print(example[1:7])
+
+print(len(example))         ## Returns Length of example
+print(12 in example)        ## Searches for element in list
+
+example.clear()           ## Clears list
+print(example)
+
+## Break and continue statements
+List = ["Anil", "Gulshan", "Bibek", "Shivendra"]
+print(List.count("Anil"))
+print(List.index("Anil"))
+
+for lIst in List:
+    if lIst == "Bibek":
+        break         ## Semicolon is optional
+    print(lIst)
+
+for liSt in List:
+    if liSt == "Bibek":
+        continue
+    print(liSt)
+
+## Using Tuple
+tuple = ("alpha", "beta", "gama") ## Declaring Tuple
+print(tuple)
+
+## Tuples are non modifiable
+Tuple = "python", "is", "case", "sensative" 
+## tuple can be define without parenthesis
+print(Tuple)
+## Accessig tuple
+itup = 0
+while itup < 3:
+    print(tuple[itup])
+    itup = itup + 1
+
+for Itup in Tuple:
+    print(Itup)
+
+
+
+## Dictionary
+data = {"code" : "python", "note" : "vim", "OS" : "Linix"}
+print(data)
+print(data["OS"])
+data["note"] = "Notepad++"
+print(data)
+## Adding data in dictionary
+data["likes"] = "hike"
+print(data)
+
+## Using modules
+from math import *  ## line id alpha 
+
+import math
+print(dir(math))    ## this line requires previous line
+root = 25
+print(sqrt(root))   ## this line require line alpha
+
