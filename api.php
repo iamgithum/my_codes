55eb16592a4d2bed7dde66e4be31dfb7cc09e51e
Shayak Rakshit <85937567+iamgithum@users.noreply.github.com>
2022-03-12 11:39:28 +0530

    Update api.php

--- api.php
+++ api.php
@@ -1,9 +1,10 @@
 
+<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 <style type=text/css rel=spreadsheet>
     input{
         border-radius:3px;
         padding:3px;
-        border-color:#6495ed;                 
+        border-color:#6495ed;
         margin-left: 6px;
     }
     button{
@@ -11,51 +12,71 @@
         align: left;
         color:#fcfcfc;
         margin-top:9px;
-        /*margin-left: -450px;*/
+        margin-left: 450px;*/
     }
 </style>
-    <form name="testleaf Form" role="form" method="post"
-          action='https://conceptninjas.com/services/web/syncLead/eUxhd25EQ3hMNXVmSWw2bkpVNFZXUT09'>
+    <form name="testLeadForm" id="testLeadForm" role="form">
         <center>
             <font color="#ff7a3d">Enquiry Form</font>
         </center>
         <fieldset>
+          <table>
+            <tr>
             <div class="form-group input-group">
-                <span class="input-group-addon">Name</span><br>
-                <input class="form-control" name="name" id="name" placeholder="Name" maxlength="50" required>
+                <td><span class="input-group-addon">Name</span></td><td>
+                <input class="form-control" name="name" id="name" placeholder="Name" maxlength="50" required></td>
             </div>
+  </tr><tr>
             <div class="form-group input-group">
-                <span class="input-group-addon">Email</span><br>
-                <input class="form-control" name="email" id="email" placeholder="Email Id" maxlength="50" required>
+            <td>    <span class="input-group-addon">Email</span></td><td>
+                <input class="form-control" name="email" id="email" placeholder="Email Id" maxlength="50" required></td>
             </div>
+            </tr><tr>
             <div class="form-group input-group">
-                <span class="input-group-addon">Mobile</span><br>
-                <input class="form-control" name="mobile" id="mobile" placeholder="Mobile" maxlength="50" required>
+             <td>   <span class="input-group-addon">Mobile</span></td><td>
+                <input class="form-control" name="mobile" id="mobile" placeholder="Mobile" maxlength="50" required></td>
             </div>
+            </tr><tr>
             <div class="form-group input-group">
-                <span class="input-group-addon">Course</span><br>
-                <input class="form-control" name="course" id="course" placeholder="Course" maxlength="50" required>
+             <td>   <span class="input-group-addon">Course</span></td><td>
+                <input class="form-control" name="course" id="course" placeholder="Course" maxlength="50" required></td>
             </div>
+            </tr><tr>
             <div class="form-group input-group">
-                <span class="input-group-addon">Location</span><br>
-                <input class="form-control" name="location" id="location" placeholder="Location" maxlength="50" required>
+               <td> <span class="input-group-addon">Location</span></td><td>
+                <input class="form-control" name="location" id="location" placeholder="Location" maxlength="50" required></td>
             </div>
+            </tr><tr>
             <div class="form-group input-group">
+              <td colspan=2>
                 <span class="input-group-addon">Message</span><br>
-                <textarea rows = "5" cols = "10" name = "message">
+                <textarea rows = "5" name = "message">
                     Optional message
-                 </textarea>
+                 </textarea></td>
             </div>
-            <div class="form-group" align="center"><a href="https://santhoshtechnologies.in">
-                <button onclick="window.location.href = 'https://santhoshtechnologies.in';"   type="submit" class="login-btn-prop btn btn-primary" id="loginBtn" name="loginBtn">
+          </tr><tr>
+            <div class="form-group" align="center">
+                <td colspan=2><button  type="submit" class="login-btn-prop btn btn-primary" id="loginBtn" name="loginBtn">
                     Submit
-                </button></a>
-
-
-                onclick="history.go(-1);"
-
-
-                
+                </button></td>
             </div>
+            </tr>
+  </table>
         </fieldset>
     </form>
+
+<script>
+    $(document).ready(function () {
+        $("#testLeadForm").submit(function (event) {
+            event.preventDefault();
+            $.ajax({
+                type: 'POST',
+                url: 'https://conceptninjas.com/services/web/syncLead/eUxhd25EQ3hMNXVmSWw2bkpVNFZXUT09',
+                data: $( "#testLeadForm" ).serialize()
+            }).done(function (data) {
+                console.log(data);
+                window.location.href = 'https://santhoshtechnologies.in';
+            });
+        });
+    });
+</script>
